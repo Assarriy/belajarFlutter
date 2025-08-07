b16a469940046431904fa1e28fb9ab93f6772d01
Assarriy <wibowoassariy23@gmail.com>
2025-08-07 09:23:37 +0700

    fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 // Contoh StatefulWidget sederhana
 class CounterApp extends StatefulWidget {
   const CounterApp({super.key});
