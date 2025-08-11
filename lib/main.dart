f40439043f374bb9a6fa8fe1ee9965f3eb22ee5f
Assarriy <wibowoassariy23@gmail.com>
2025-08-11 09:19:36 +0700

    materi bab 4, management state sederhana

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,24 @@
 import 'package:flutter/material.dart';
+// Import file lainnya
+import 'pages/bab3/stateful.dart';
+import 'pages/bab4/materi.dart';
 
-// Contoh StatefulWidget sederhana
-class CounterApp extends StatefulWidget {
-  const CounterApp({super.key});
-  @override
-  State<CounterApp> createState() => _CounterAppState(); //
+void main() {
+  runApp(const MyApp());
 }
 
-class _CounterAppState extends State<CounterApp> {
-  int _counter = 0; // State yang dapat berubah
-  void _incrementCounter() {
-    setState(() {
-      // Metode setState() memberitahu Flutter untuk
-      _counter++;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Stateful Widget Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Anda telah menekan tombol sebanyak:',
-                style: TextStyle(fontSize: 18),
-              ),
-              Text(
-                '$_counter', // Menampilkan state _counter
-                style:
-                    const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20), // Memberi jarak
-              ElevatedButton(
-                onPressed: _incrementCounter, // Memanggil fungsi
-
-                child: const Text('Tambah'),
-              ),
-            ],
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'bab4',
+      routes: {
+        'bab3': (context) => Stateful(),
+        'bab4': (context) => Button(),
+      },
     );
   }
-}
-
-void main() {
-  runApp(const CounterApp());
 }
