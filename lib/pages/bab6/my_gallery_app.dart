0f522e21c7e525508e0e225ea865819f5a070032
Alif <alifsatriamandala14@gmail.com>
2025-08-18 08:29:25 +0700

    a_product_app

---
+++ lib/pages/bab6/my_gallery_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bab6Page extends StatelessWidget {
+  const Bab6Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Bab 6")),
+      body: const Center(
+        child: Text("Ini adalah halaman Bab 6 (masih kosong)"),
+      ),
+    );
+  }
+}
